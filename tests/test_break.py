7b5effd83b04ad5db19695b16fb836469509ae7a
petrovska-petro <84875062+petrovska-petro@users.noreply.github.com>
2021-10-23 11:58:54 +0100

    PoC bug in reward tracking

---
+++ tests/test_break.py
@@ -0,0 +1,31 @@
+from helpers.constants import *
+from brownie import Wei, chain
+
+
+def test_break(deployer, vaults, badger_tree, want_whale, want, accounts):
+    want.approve(vaults[0], MaxUint256, {"from": want_whale})
+
+    badger_tree.add(vaults[0], [], {"from": deployer})
+
+    # accounts[0] is _scheduler & _pauser, 7200 blocks
+    badger_emitted = 100
+    badger_tree.addSettRewards(
+        vaults[0], 7200, [Wei(f"{badger_emitted} ether")], {"from": accounts[0]}
+    )
+
+    print(want.balanceOf(want_whale))
+
+    # user 1st deposit
+    vaults[0].deposit(want.balanceOf(want_whale) * 0.05, {"from": want_whale})
+
+    chain.mine(100)
+
+    reward_pending_before = badger_tree.pendingRewards(vaults[0], want_whale)[0]
+
+    # same user 2nd deposit
+    vaults[0].deposit(want.balanceOf(want_whale) * 0.05, {"from": want_whale})
+
+    reward_pending_after = badger_tree.pendingRewards(vaults[0], want_whale)[0]
+    
+    # should be higher badger rewards, right? TOFIX!
+    assert reward_pending_after > reward_pending_before
