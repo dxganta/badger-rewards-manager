82aaf2cd66cc0594aa2abbb2643ec8d5bfbb6a53
realdiganta <digantakalita.ai@gmail.com>
2021-10-26 23:58:20 +0530

    transfer tests added

--- tests/test_deposit_withdraw_flow.py
+++ tests/test_deposit_withdraw_flow.py
@@ -2,10 +2,9 @@ from brownie import *
 from brownie import (
     interface
 )
-import brownie
 from helpers.constants import *
 from helpers.utils import *
-from config import DAI, CRV, LINK
+from config import DAI, CRV
 
 
 def test_deposit_withdraw_rewards(deployer, users, vaults, badger_tree, badger, want):
@@ -129,3 +128,63 @@ def test_deposit_withdraw_rewards(deployer, users, vaults, badger_tree, badger, 
                   (crv_amount * 0.6 * u1_prcnt_new) - (crv_amount/blocks * u1_prcnt_new) + (crv_amount/blocks * u1_prcnt), 0.001)
     assert approx(pd2b[2], (crv_amount * 0.4 * u2_prcnt) +
                   (crv_amount * 0.6 * u2_prcnt_new) - (crv_amount/blocks * u2_prcnt_new) + (crv_amount/blocks * u2_prcnt), 0.001)
+
+    for i in range(0, 2):
+        badger_tree.claim(vault, users[i], [0, 1, 2], {"from": users[i]})
+
+    # testing the transfer of tokens
+    # transfer 30% shares of user1 to user3
+    vault.transfer(
+        users[2],  0.3 * vault.balanceOf(users[0]), {"from": users[0]})
+
+    # now for the third cycle user1 must get 70% of his previous share & the user3 must get the rest 30%
+    # 4th cycle
+    blocks = 50
+    badger_amount = 30 * 10**18
+    dai_amount = 1500 * 10**18
+    crv_amount = 40 * 10**18
+    badger.transfer(badger_tree, badger_amount, {"from": deployer})
+    dai.transfer(badger_tree, dai_amount, {"from": deployer})
+    crv.transfer(badger_tree, crv_amount, {"from": deployer})
+    badger_tree.addSettRewards(
+        vault, blocks, [badger_amount, dai_amount, crv_amount], {"from": deployer})
+
+    chain.mine(blocks)
+
+    pd1 = badger_tree.pendingRewards(vault, users[0])
+    pd2 = badger_tree.pendingRewards(vault, users[1])
+    pd3 = badger_tree.pendingRewards(vault, users[2])
+
+    assert approx(pd1[0], badger_amount * 0.7 * u1_prcnt_new, 0.001)
+    assert approx(pd2[0], badger_amount * u2_prcnt_new, 0.001)
+    assert approx(pd3[0], badger_amount * 0.3 * u1_prcnt_new, 0.001)
+
+    assert approx(pd1[1], dai_amount * 0.7 * u1_prcnt_new, 0.001)
+    assert approx(pd2[1], dai_amount * u2_prcnt_new, 0.001)
+    assert approx(pd3[1], dai_amount * 0.3 * u1_prcnt_new, 0.001)
+
+    assert approx(pd1[2], crv_amount * 0.7 * u1_prcnt_new, 0.001)
+    assert approx(pd2[2], crv_amount * u2_prcnt_new, 0.001)
+    assert approx(pd3[2], crv_amount * 0.3 * u1_prcnt_new, 0.001)
+
+    for i in range(0, 3):
+        badger_tree.claim(vault, users[i], [0, 1, 2], {"from": users[i]})
+
+    # final withdraw tests
+    # user3 withdraws everything & stops getting rewards
+    vault.withdrawAll({"from": users[2]})
+    blocks = 50
+    badger_amount = 1 * 10**18
+    dai_amount = 10 * 10**18
+    crv_amount = 1 * 10**18
+    badger.transfer(badger_tree, badger_amount, {"from": deployer})
+    dai.transfer(badger_tree, dai_amount, {"from": deployer})
+    crv.transfer(badger_tree, crv_amount, {"from": deployer})
+    badger_tree.addSettRewards(
+        vault, blocks, [badger_amount, dai_amount, crv_amount], {"from": deployer})
+
+    chain.mine(blocks)
+
+    pd3 = badger_tree.pendingRewards(vault, users[2])
+
+    assert pd3 == (0, 0, 0)
