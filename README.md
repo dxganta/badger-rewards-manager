a9f65938611c4adda5f533fa98f3005ee9fd7d44
realdiganta <digantakalita.ai@gmail.com>
2021-10-29 16:49:16 +0530

    production deploy filed added

--- README.md
+++ README.md
@@ -3,8 +3,6 @@
 
 # Tests
 
-2. Add new reward Token & Tests for it.
-
 3. Also add a production_deploy.sol file
 
 4. Deploy on ropsten/goerli & test that you can check the reward tokens for a set from the explorer.
---
+++ scripts/production_deploy.py
@@ -0,0 +1,36 @@
+from brownie import (
+    accounts,
+    BadgerTreeV3,
+)
+from config import PAUSER, SCHEDULER
+from brownie import *
+from dotmap import DotMap
+
+import click
+from rich.console import Console
+
+console = Console()
+
+
+def main():
+    owner = connect_account()
+
+    # Deploy rewards contract
+    badger_tree = BadgerTreeV3.deploy(
+        SCHEDULER,
+        PAUSER,
+        {"from": owner}
+    )
+
+    return DotMap(
+        deployer=owner,
+        badger_tree=badger_tree,
+    )
+
+
+def connect_account():
+    click.echo(f"You are using the '{network.show_active()}' network")
+    dev = accounts.load(click.prompt(
+        "Account", type=click.Choice(accounts.load())))
+    click.echo(f"You are using: 'dev' [{dev.address}]")
+    return dev
